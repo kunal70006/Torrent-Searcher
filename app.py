e60aa12a985492975c503499ba438679d4e92be3
Kunal <singhkunaldeep@gmail.com>
2020-04-29 17:53:35 +0530

    Made it noob friendly

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import time
 from tpblite import TPB
 
 t = TPB()
 file = open("Torrent List.txt", "w+")
 torrents = input("Search for a torrent: ")
 torrentList = t.search(torrents)
+
 for torrent in torrentList:
     file.write(str(torrent) + "\n")
     file.write(str(torrent.magnetlink) + "\n\n")
+
+print("Search complete, writing into a text file")
+time.sleep(5)
