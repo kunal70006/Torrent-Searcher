ab7d27da28d0204dace00f80387ce29bd706aef8
Kunal <singhkunaldeep@gmail.com>
2020-04-29 17:45:10 +0530

    Writing torrent into a txt file

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from tpblite import TPB
 
-
 t = TPB()
+file = open("Torrent List.txt", "w+")
 torrents = input("Search for a torrent: ")
 torrentList = t.search(torrents)
 for torrent in torrentList:
-    print(torrent.magnetlink)
-
+    file.write(str(torrent) + "\n")
+    file.write(str(torrent.magnetlink) + "\n\n")
